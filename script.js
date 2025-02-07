02abd3d901b39beb21037e79e2c1eaab77c6f4ae
EdersonHTT <edersondasilvaschmidt@gmail.com>
2025-02-07 17:28:25 -0300

    Especificando input

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const inputNu = document.getElementById('CEP')
+
+inputNu.addEventListener('keydown', (e)=>{
+    let cep = ""
+    if(e.key != 'Backspace'){
+        if(inputNu.value.length > 0){
+            cep += inputNu.value
+            if(cep.length === 5){
+                inputNu.value += "-"
+            } else if (cep.length === 9){
+                cep = cep.slice(0, -1)
+                inputNu.value = cep
+            }
+        }
+    }
+})
