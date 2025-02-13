5832ef3d8d95a09d10ad36d55beccd75f7f7e7cd
EdersonHTT <edersondasilvaschmidt@gmail.com>
2025-02-13 17:48:54 -0300

    Feito a validação de input

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-const inputNu = document.getElementById('CEP')
-
-inputNu.addEventListener('keydown', (e)=>{
-    let nu = ''
-    if(e.key != 'Backspace'){
-        if(inputNu.value.length > 0){
-            nu += inputNu.value
-            if(nu.length === 5){
-                inputNu.value += "-"
-            } else if (nu.length === 9){
-                nu = nu.slice(0, -1)
-                inputNu.value = nu
-            }
-        }
-    }
-})
-
 // Uso da API para consultar cep
 
 const botao = document.getElementById('botaoProcurar')
 botao.addEventListener("click", async () =>{
-    const cep = `${inputNu.value}`
-    cep.replace('-', '')
+    let cep = `${inputNu.value}`
+    cep = cep.replace('-', '')
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     const responseCep = await response.json()
 
